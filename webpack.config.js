5578bb9046278de5876fc68ce3766c8a0fbb8e79
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2022-11-22 15:08:22 +0900

    webpack setup complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   entry: "./src/client/js/main.js",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  mode: "development",
   output: {
-    path: path.resolve(__dirname, "assets", "js"),
-    filename: "main.js",
+    path: path.resolve(__dirname, "assets"),
+    filename: "js/main.js",
+    clean: true,
   },
   module: {
     rules: [
@@ -17,6 +25,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
