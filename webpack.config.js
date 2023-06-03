086a9419c402cf77f8695ba21f9f78dedea72d0c
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2023-06-03 21:00:48 +0900

    build script

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,11 @@ module.exports = {
     recorder: BASE_JS + "recorder.js",
     commentSection: BASE_JS + "commentSection.js",
   },
-  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
   ],
-  mode: "development",
   output: {
     path: path.resolve(__dirname, "assets"),
     filename: "js/[name].js",
