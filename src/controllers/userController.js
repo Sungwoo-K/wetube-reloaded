dd7b335d1c2abc71c18df3ea49cd6ffbe7e4a75a
Sungwoo-K <72499989+Sungwoo-K@users.noreply.github.com>
2023-06-06 22:18:56 +0900

    client_id change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,7 +64,7 @@ export const postLogin = async (req, res) => {
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id: "4bb346773f5471751521",
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
@@ -76,7 +76,7 @@ export const startGithubLogin = (req, res) => {
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
-    client_id: "4bb346773f5471751521",
+    client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
     code: req.query.code,
   };
